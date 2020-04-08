f9be89d5db36e31c56e74e25c508090cb9ead872
Cosmin <cosmin@fasttrackit.org>
2020-04-08 20:22:30 +0300

    test3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("test");
+        
         char myChar = '@';
         byte myByte = 127;
         short myShort = 1231;
