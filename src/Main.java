b46326370ff139635622a8ca0ddf49c1663e1f3e
Cosmin <cosmin@fasttrackit.org>
2020-04-08 20:19:49 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!3");
 
         char myChar = '@';
         byte myByte = 127;
